3021b4ca44d105761a11c43100526fca64e408ab
himanoa <matsunoappy@gmail.com>
2020-03-25 17:39:05 +0900

    次のサイトへのリンクを追加
    - FANBOX
    - もちのあちゃんのお店
    - マシュマロh

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -23,6 +23,24 @@ const Home: FunctionalComponent = () => {
                     https://github.com/MochiNoaProject/homepage
                 </a>
             </p>
+            <h2>FANBOX</h2>
+            <p>
+                <a href="https://www.pixiv.net/fanbox/creator/49400494">
+                    https://www.pixiv.net/fanbox/creator/49400494
+                </a>
+            </p>
+            <h2>もちのあちゃんのお店</h2>
+            <p>
+                <a href="https://mochinoa.booth.pm/">
+                    https://mochinoa.booth.pm/
+                </a>
+            </p>
+            <h2>マシュマロ</h2>
+            <p>
+                <a href="https://marshmallow-qa.com/_noach">
+                    https://marshmallow-qa.com/_noach
+                </a>
+            </p>
         </div>
     );
 };
