9087326efd012990cc4bbfb89b408371c36d0596
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-04-01 23:27:46 +0900

    chore: remove baseroute.ts
    * nowに移行したため不要

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,30 +1,29 @@
 import { FunctionalComponent, h } from "preact";
 import { Link } from "preact-router/match";
 import * as style from "./style.css";
-import baseroute from "../../baseroute";
 
 const Header: FunctionalComponent = () => {
     return (
         <div class={style.headerWrapper}>
             <header class={style.header}>
                 <p class={style.logo}>
-                    <Link href={`${baseroute}/`}>
+                    <Link href={`/`}>
                         <img src="/assets/logo.png" alt="Logo"/>
                     </Link>
                 </p>
                 <nav>
-                    <Link activeClassName={style.active} href={`${baseroute}/`}>
+                    <Link activeClassName={style.active} href={`/`}>
                         Home
                     </Link>
                     <Link
                         activeClassName={style.active}
-                        href={`${baseroute}/profile`}
+                        href={`/profile`}
                     >
                         Me
                     </Link>
                     <Link
                         activeClassName={style.active}
-                        href={`${baseroute}/profile/mochinoa`}
+                        href={`/profile/mochinoa`}
                     >
                         Mochinoa
                     </Link>
