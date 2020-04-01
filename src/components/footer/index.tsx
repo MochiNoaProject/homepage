a99d58bc6045674fe5cea8b8c1f5070fa7b9455d
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-04-01 23:31:07 +0900

    fix: 不要なimportの削除

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,5 @@
 import { FunctionalComponent, h } from "preact";
-import { Link } from "preact-router/match";
 import * as style from "./style.css";
-import baseroute from "../../baseroute";
 
 const Header: FunctionalComponent = () => {
     return (
