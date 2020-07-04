e60e48f263946881b87cc393e17f82775f1f7ab6
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-07-05 05:35:53 +0900

    fix(ogp): set favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,6 +19,27 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
                     rel="apple-touch-icon"
                     href="/img/icons/apple-touch-icon.png"
                 />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/img/icons/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/img/icons/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="shortcut icon" href="/img/favicon.ico" />
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:site" content="@_noach" />
+                <meta name="twitter:title" content="望月のあ Official Site" />
+                <meta
+                    name="twitter:image"
+                    content="https://mochinoa.now.sh/img/ogp.png"
+                />
             </Head>
             <Component {...pageProps} />
         </>
