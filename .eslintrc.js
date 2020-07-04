cc36d9a3d7719564058a9c4f790127ea1a5299a8
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-07-04 23:52:37 +0900

    chore(eslint): make sure linting correctly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,41 @@
 module.exports = {
     env: {
-        browser: true
+        es6: true,
+        amd: true,
+        node: true,
     },
-    plugins: ["@typescript-eslint"],
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:prettier/recommended",
-        "prettier/@typescript-eslint",
-        "prettier/react"
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        project: "./tsconfig.eslint.json",
-    },
-    rules: {
-        "react/no-unknown-property": ["error", { ignore: ["class"] }],
-    },
-    settings: {
-        react: {
-            pragma: "h",
-            version: "detect"
-        },
-    },
+    extends: ["eslint:recommended", "plugin:prettier/recommended"],
     overrides: [
         {
-            files: ["*.js"],
+            files: ["*.ts", "*.tsx"],
+            env: {
+                browser: true,
+            },
+            plugins: ["@typescript-eslint"],
+            extends: [
+                "plugin:@typescript-eslint/eslint-recommended",
+                "plugin:@typescript-eslint/recommended",
+                "plugin:@typescript-eslint/recommended-requiring-type-checking",
+                "plugin:react/recommended",
+                "plugin:react-hooks/recommended",
+                "prettier/@typescript-eslint",
+                "prettier/react",
+            ],
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                project: "./tsconfig.eslint.json",
+            },
             rules: {
-                "@typescript-eslint/explicit-function-return-type": "off",
-            }
-        }
-    ]
+                "react/no-unknown-property": ["error", { ignore: ["class"] }],
+            },
+            settings: {
+                react: {
+                    pragma: "h",
+                },
+            },
+        },
+    ],
 };
